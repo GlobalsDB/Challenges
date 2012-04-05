cc043bd12ba7f45da6a4c3a7be9d4cae0ee308c0
Ron Sweeney <ron.sweeney@gmail.com>
2012-04-05 11:27:32 -0400

    Challenge Submission, Initial - LAX

---
+++ 4/sween/dicom2global/dicom2global/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace dicom2global
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(264, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse DICOM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 257);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(379, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load 2 Global";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 308);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(483, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 330);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "dicom2global";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ 4/sween/dicom2global/dicom2global/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Dicom;
+using Dicom.Data;
+using Dicom.IO;
+using InterSystems.Globals;
+
+
+namespace dicom2global
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /// Browse for a DICOM File
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /// Globals Connectivity Here
+            /// 
+            Connection connection = null;
+         try {
+            
+            connection = ConnectionContext.GetConnection();
+
+           
+           if (!connection.IsConnected()) {
+               connection.Connect();
+            }
+
+           // Open the DICOM File
+
+            DcmDictionary.ImportDictionary("dicom.dic");
+            DcmDictionary.ImportDictionary("private.dic");
+
+             var dcmFile = new DicomFileFormat();
+             var dcmFileName = textBox1.Text.ToString();
+             if (dcmFile.Load(dcmFileName, DicomReadOptions.Default) == DicomReadStatus.Success)
+             {
+
+                 var metainfos = dcmFile.FileMetaInfo.Recurse();
+
+                 MessageBox.Show(metainfos.Count().ToString());
+
+             }
+            
+
+           // Need to grab the study instance UID and that will be our Root Node
+
+           // NodeReference studyinstanceuid = connection.CreateNodeReference("dicom");
+            
+           // studyinstanceuid.Set(".1.4.7.2.45.6.9");
+
+           // MetaData Information to meta collection
+
+
+           // DataSet Information to dataset collection
+           
+              
+             }
+             
+             catch(Exception ee) {
+
+                 MessageBox.Show(ee.ToString());
+             }
+             
+        }
+    }
+}
